f099b033fc5c9fc71bbf34d3f6f3d15b06db499e
CSY2022 <tel19807034568@outlook.com>
2025-04-12 20:06:02 +0800

    Try to fix bingwallpaper

--- assets/js/bing.js
+++ assets/js/bing.js
@@ -2,7 +2,7 @@ const https = require("https");
 const fs = require("fs");
 
 const options = {
-  hostname: "www.bing.com",
+  hostname: "cn.bing.com",
   port: 443,
   path: "/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=zh-CN",
   method: "GET",
